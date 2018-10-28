ff66974e9932ecac1d9f29f8e2f332da14bc382b
Arslan <yamamoto1659@gmail.com>
2018-10-28 14:16:17 +0300

    properties encoding filter removed

--- src/main/java/com/arslan/homefin_server/security/config/SecurityConfig.java
+++ src/main/java/com/arslan/homefin_server/security/config/SecurityConfig.java
@@ -61,11 +61,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-//        filter.setEncoding("UTF-8");
-//        filter.setForceEncoding(true);
-//        http.addFilterBefore(filter, WebAsyncManagerIntegrationFilter.class);
-
         http
                 .headers().frameOptions().disable().and()
                 .cors()
@@ -96,7 +91,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .authenticated();
 
         // Add our custom JWT security filter
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+      //  http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
 }
