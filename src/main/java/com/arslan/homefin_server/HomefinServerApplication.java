0d1623a0c8d585108fce6b1f0575101ab38fa15f
Arslan <yamamoto1659@gmail.com>
2018-10-28 14:56:49 +0300

    properties encoding filter removed

--- src/main/java/com/arslan/homefin_server/HomefinServerApplication.java
+++ src/main/java/com/arslan/homefin_server/HomefinServerApplication.java
@@ -3,13 +3,13 @@ package com.arslan.homefin_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.FilterChainProxy;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 
-import javax.servlet.Filter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HomefinServerApplication {
@@ -17,5 +17,10 @@ public class HomefinServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(HomefinServerApplication.class, args);
     }
-
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
+        converter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "plain", Charset.forName("UTF-8"))));
+        return converter;
+    }
 }
