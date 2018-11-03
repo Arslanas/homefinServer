a65bafa2a275b163d70418cf28e6288cf0542a29
Arslan <yamamoto1659@gmail.com>
2018-11-03 12:07:59 +0300

    jpa config for utf

---
+++ src/main/java/com/arslan/homefin_server/RootConfig.java
@@ -0,0 +1,80 @@
+package com.arslan.homefin_server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = {"homefin_server"})
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"homefin_server"})
+@PropertySource(value = "classpath:application.properties")
+public class RootConfig {
+
+    @Autowired
+    Environment env;
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUsername(env.getProperty("db.user"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        return dataSource;
+    }
+
+
+    @Bean(name = "jpaVendorAdapter")
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        adapter.setShowSql(true);
+        return adapter;
+    }
+
+
+    @Bean
+    @Autowired
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(JpaVendorAdapter adapter, DataSource dataSource){
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.format_sql", String.valueOf(true));
+        properties.setProperty("hibernate.connection.CharSet","utf8");
+        properties.setProperty("hibernate.connection.characterEncoding","utf8");
+        properties.setProperty("hibernate.connection.useUnicode", String.valueOf(true));
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setJpaProperties(properties);
+        emf.setJpaVendorAdapter(adapter);
+        emf.setPackagesToScan("homefin_server");
+        return emf;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public BeanPostProcessor persistenceTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
